883bfcac351c09ea481634bac97e1bfbad464fee
Emmanuel Texier <emmanuel.texier@magnet.com>
2014-09-20 08:54:20 -0700

    Adding pending step

---
+++ tests/src/test/java/com/magnet/tools/tests/SimpleJavaClientSteps.java
@@ -0,0 +1,15 @@
+package com.magnet.tools.tests;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by etexier on 9/20/14.
+ */
+public class SimpleJavaClientSteps {
+  @When("^I run the simple java client with options:$")
+  public void I_run_the_simple_java_client_with_options(String options) throws Throwable {
+    // Express the Regexp above with the code you wish you had
+    throw new PendingException();
+  }
+}
